c5c423f2bd2f9095028442532a2b218e9d630228
Anwar Hussen <anwar.hussen4@gmail.com>
2017-08-26 16:05:26 +0600

    flash message added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,23 +21,25 @@
 
         @include('includes.header')
 
+        <div class="row">
+            <div class="container">
+                @include('flash::message')
+            </div>
+        </div>
+
         @yield('content')
 
         @include('includes.footer')
 
     </div>
 
-    {{-- <script type="text/javascript">
-    function googleTranslateElementInit() {
-        new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'bn,en', layout:  google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-        }
-    </script>
-    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> --}}
-
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/slide.js') }}"></script>
-    
+    <script type="text/javascript">
+        $('#flash-overlay-modal').modal();
+    </script>
+
     @stack('scripts')
 </body>
 </html>
